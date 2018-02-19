d11782c7cbdc8bff5052b1dc84613c25d182c059
Adam Magee <magee927@gmail.com>
2018-02-19 09:09:00 -0500

    all working except for delete route which isnt MVP

--- models/burgerModel.js
+++ models/burgerModel.js
@@ -17,7 +17,12 @@ var burger = {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
-  }
+  },
+  delete: function (condition, cb) {
+    orm.delete("burgers", condition, function (res) {
+        cb(res);
+    });
+}
 };
 
 // Export the database functions for the controller (catsController.js).
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,44 +1,60 @@
-
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+$(function () {
+
+  $(".devour-burger").on("click", function (event) {
+    var id = $(this).data("id");
+    var devouredState = $(this).data("devouredState");
+    console.log(devouredState);
+    var newDevouredState = {
+      devoured: true
+    };
+    console.log(newDevouredState);
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function () {
+        console.log("changed devoured state to", newDevouredState);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
     
-    $(".devour-burger").on("click", function(event) {
-      var id = $(this).data("id");
-      var devouredState = $(this).data("devouredState");
-      console.log(devouredState);
-      var newDevouredState = {devoured: true};
-      console.log(newDevouredState);
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed devoured state to", newDevouredState);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        name: $("#burger_name").val().trim(),
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
   });
+  $(".delete-burger").on("click", function (event) {
+    var id = $(this).data("id");
+    delete(id)
+    console.log(id);
+    $.ajax("/api/burgers/" + id,{
+      type: "DELETE",
+      
+    }).then(
+      function() {
+        console.log("deleted Burger", id);
+      location.reload();
+    })
+  })
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burger_name").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+  
+})
