dd14264399419500cd8c0aea6b9bded4885fddb3
Adam Magee <magee927@gmail.com>
2018-02-20 11:00:35 -0500

    read me update

--- config/connection.js
+++ config/connection.js
@@ -3,31 +3,20 @@ var mysql = require("mysql");
 
 var connection; 
 
-var connection;
-
 if (process.env.JAWSDB_URL) {
     console.log("USING JAWS");
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port: 3306,
-        host: process.env.HOST,
-        user: process.env.DBUSER,
-        password: process.env.DBPASS,
+      port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "suorcidul1",
         database: "burgers_db"
     });
 }
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "suorcidul1",
-//   database: "burgers_db"
-// });
+
 
 // Make connection.
 connection.connect(function(err) {
@@ -40,7 +29,6 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
-}
 
 // Set up MySQL connection.
 // var mysql = require("mysql");
